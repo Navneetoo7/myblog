d031699190ed275c2b464b9097627044b7a532c3
Navneet007 <navneetjustin@gmail.com>
2021-01-09 23:50:50 +0530

    need update

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('dashboard/', views.dashboardView, name="dashboard"),
     path('login/',LoginView.as_view(),name="login_url"),
     path('register/',views.registerView, name="register_url"),
-    path('logout/',LogoutView.as_view(next_page="dashboard"), name="Logout"),
+    path('logout/',LogoutView.as_view(next_page="home"), name="Logout"),
 ]
 
